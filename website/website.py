6043360da95e46e6fe3690e77f6e49b02881ff50
Mikayla Campbell <hewantshiscar@Mikaylas-MBP.hsd1.or.comcast.net>
2021-02-25 18:10:44 -0800

    Fixed location

--- website/website.py
+++ website/website.py
@@ -1,33 +1,33 @@
 from flask import Flask, send_from_directory
 import os
 
-TEMPLATE_DIR = os.path.abspath('/Desktop/433Website/')
+TEMPLATE_DIR = os.path.abspath('/Desktop/website/')
 
 app = Flask(__name__, static_folder='')
 
 @app.route('/')
 def home():
-	root_dir = os.path.dirname(os.getcwd()) + '/433Website'
+	root_dir = os.path.dirname(os.getcwd()) + '/website'
 	return send_from_directory(root_dir, 'testWebsite.html')
 
 @app.route('/voteCount')
 def voteCount():
-	root_dir = os.path.dirname(os.getcwd()) + '/433Website'
+	root_dir = os.path.dirname(os.getcwd()) + '/website'
 	return send_from_directory(root_dir, 'voteCount.html')
 
 @app.route('/signIn')
 def signIn():
-	root_dir = os.path.dirname(os.getcwd()) + '/433Website'
+	root_dir = os.path.dirname(os.getcwd()) + '/website'
 	return send_from_directory(root_dir, 'signIn.html')
 
 @app.route('/end')
 def end():
-	root_dir = os.path.dirname(os.getcwd()) + '/433Website'
+	root_dir = os.path.dirname(os.getcwd()) + '/website'
 	return send_from_directory(root_dir, 'end.html')
 
 @app.route('/castVote')
 def castVote():
-	root_dir = os.path.dirname(os.getcwd()) + '/433Website'
+	root_dir = os.path.dirname(os.getcwd()) + '/website'
 	return send_from_directory(root_dir, 'castVote.html')
 
 if __name__ == "__main__":
