ea84da09edf9143c4c3bdd78a25cb636a35c2a94
ntitzler1 <ntitzler@gmail.com>
2021-03-01 18:50:11 -0800

    new election

---
+++ election/truffle-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+      gas: 4710000
+    },
+    develop: {
+      port: 8545
+    }
+  }
+};
